e15248294bc5872b3376e915ac88e1ccadc81d2b
Student User <student@workstation.lab.example.com>
2020-08-09 10:27:51 +0000

    a

--- test/test.py
+++ test/test.py
@@ -4,3 +4,7 @@ import sys
 
 os.environ['test'] = "-------------------abc-------------------------------" 
 print(os.getenv('test'))
+
+file = open("test.txt", "w") 
+file.write("Your text goes here") 
+file.close() 
