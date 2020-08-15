2bea2f06d319cf927ef4f7196c1ad22270dd828b
superchi superchi <superchi@aol.com>
2020-08-15 05:07:19 -0400

    abc

--- test/test.py
+++ test/test.py
@@ -5,6 +5,6 @@ import sys
 os.environ['test'] = "-------------------abc-------------------------------" 
 print(os.getenv('test'))
 
-file = open("/tmp/test.txt", "w") 
+file = open("/tmp/hello.txt", "w") 
 file.write("Your text goes here") 
 file.close() 
