756b83ca2505977794056c582cf1f66b00991764
Student User <student@workstation.lab.example.com>
2020-08-15 23:09:39 +0000

    test

--- test/test.py
+++ test/test.py
@@ -3,8 +3,9 @@ import subprocess
 import sys
 
 os.environ['test'] = "-------------------abc-------------------------------" 
-print(os.getenv('test'))
 
 file = open("/tmp/hello.txt", "w") 
 file.write("Your text goes here") 
 file.close() 
+
+os.environ['test'] = 'kevin'
